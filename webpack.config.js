92530b97592ac1bb6753fcae65b36ad7ab174442
Thomas Bullock laptop <motbook@motbooks-MacBook-Pro.local>
2018-03-19 22:16:58 +1100

    styling hearts

--- webpack.config.js
+++ webpack.config.js
@@ -69,8 +69,9 @@ const config = {
     rules: [javascript, styles]
   },
   // finally we pass it an array of our plugins - uncomment if you want to uglify
-  // plugins: [uglify]
+  // plugins: [uglify],
   plugins: [
+    uglify,
     // here is where we tell it to output our css to a separate file
     new ExtractTextPlugin('style.css'),
   ],
