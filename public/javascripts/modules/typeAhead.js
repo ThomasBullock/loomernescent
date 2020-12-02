ad882235a503746a3676b61745b349596cdd779a
Thomas Bullock <motbollox@gmail.com>
2020-12-02 18:24:54 +1100

    update to atlas and cleanup logs

--- public/javascripts/modules/typeAhead.js
+++ public/javascripts/modules/typeAhead.js
@@ -1,92 +1,96 @@
-import axios from 'axios';
-import dompurify from 'dompurify';
+import axios from "axios";
+import dompurify from "dompurify";
 
 function searchResultsHTML(results, type) {
-	const path = (type === 'name') ? 'band' : 'album';
-	return results.map(item => {
-		return `
+  const path = type === "name" ? "band" : "album";
+  return results
+    .map((item) => {
+      return `
 			<a href="/${path}/${item.slug}" class="search__result">
 				<strong>${item[type]}</strong>
 			</a>
 		`;
-	}).join('')
+    })
+    .join("");
 }
 
-
 function typeAhead(search) {
-	if(!search) return;
-	
-	const searchInput = search.querySelector('input[name="search"]');
-	const searchResults = search.querySelector('.search__results');
-	
-	// console.log(searchInput, searchResults)
-	
-	searchInput.addEventListener('input', function() {
-		// if there is no value, hide results and return!
-		if(!this.value) {
-			searchResults.style.display = 'none';
-			return
-		} 
+  if (!search) return;
 
-		// show the search results!
-		searchResults.style.display = 'block';
-	
-		axios
-			.get(`/api/v1/search?q=${this.value}`)
-			.then(response => {
-				if(response.data.length) {
-					console.log(response.data)
-					const responseType = (response.data[0].hasOwnProperty('name')) ? 'name' : 'title';
-					console.log(responseType)
-					const html = dompurify.sanitize(searchResultsHTML(response.data, responseType));
-					searchResults.innerHTML = html;
-					return;
-				} 
-				// tell them nothing came back
-				if(this.value.length > 3) {
-					searchResults.innerHTML = dompurify.sanitize(
-						`<span class="search__result"><strong>No results found for ${this.value}</strong></span>`
-						);					
-				}		
-			})
-			.catch(err => {
-				console.error(err)
-			});
-	}); 
-	
-	// handle keyboard inputs
-	searchInput.addEventListener('keyup', (e) => {
-		console.log(e.keyCode);
-		// if they aren't pressing up, down or enter then ignore!
-		if(![38, 40, 13].includes(e.keyCode)) {
-			return; // exit!
-		}
-		
-		const activeClass = 'search__result--active';
-		const current = searchResults.querySelector(`.${activeClass}`);
-		const items = search.querySelectorAll('.search__result')
-		let next;
-		// console.log(items);
-		if(e.keyCode === 40 && current) { // they press down and there is a current selected
-			next = current.nextElementSibling || items[0]; // go to nextSibling and if there isn't one goto 0
-		} else if (e.keyCode === 38 && current) {
-			next = current.previousElementSibling || items[items.length - 1]; // go to prevSibling and if there isn't one goto end
-		} else if (e.keyCode === 40) {
-			next = items[0];
-		} else if (e.keyCode === 38) {
-			next = items[items.length - 1];
-		} else if (e.keyCode === 13 && current.href) {
-			window.location = current.href;
-			return;
-		}
-		
-		next.classList.add(activeClass);
-		
-		if(current) {
-			current.classList.remove(activeClass);
-		}
-		console.log('Do Something!');
-	})
+  const searchInput = search.querySelector('input[name="search"]');
+  const searchResults = search.querySelector(".search__results");
+
+  // console.log(searchInput, searchResults)
+
+  searchInput.addEventListener("input", function () {
+    // if there is no value, hide results and return!
+    if (!this.value) {
+      searchResults.style.display = "none";
+      return;
+    }
+
+    // show the search results!
+    searchResults.style.display = "block";
+
+    axios
+      .get(`/api/v1/search?q=${this.value}`)
+      .then((response) => {
+        if (response.data.length) {
+          console.log(response.data);
+          const responseType = response.data[0].hasOwnProperty("name")
+            ? "name"
+            : "title";
+          const html = dompurify.sanitize(
+            searchResultsHTML(response.data, responseType)
+          );
+          searchResults.innerHTML = html;
+          return;
+        }
+        // tell them nothing came back
+        if (this.value.length > 3) {
+          searchResults.innerHTML = dompurify.sanitize(
+            `<span class="search__result"><strong>No results found for ${this.value}</strong></span>`
+          );
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
+
+  // handle keyboard inputs
+  searchInput.addEventListener("keyup", (e) => {
+    // if they aren't pressing up, down or enter then ignore!
+    if (![38, 40, 13].includes(e.keyCode)) {
+      return; // exit!
+    }
+
+    const activeClass = "search__result--active";
+    const current = searchResults.querySelector(`.${activeClass}`);
+    const items = search.querySelectorAll(".search__result");
+    let next;
+    // console.log(items);
+    if (e.keyCode === 40 && current) {
+      // they press down and there is a current selected
+      next = current.nextElementSibling || items[0]; // go to nextSibling and if there isn't one goto 0
+    } else if (e.keyCode === 38 && current) {
+      next = current.previousElementSibling || items[items.length - 1]; // go to prevSibling and if there isn't one goto end
+    } else if (e.keyCode === 40) {
+      next = items[0];
+    } else if (e.keyCode === 38) {
+      next = items[items.length - 1];
+    } else if (e.keyCode === 13 && current.href) {
+      window.location = current.href;
+      return;
+    }
+
+    next.classList.add(activeClass);
+
+    if (current) {
+      current.classList.remove(activeClass);
+    }
+    console.log("Do Something!");
+  });
 }
 
 export default typeAhead;
--- start.js
+++ start.js
@@ -1,32 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // import environmental variables from our variables.env file
-require('dotenv').config({ path: 'variables.env' });
+require("dotenv").config({ path: "variables.env" });
 
 // Connect to our Database and handle an bad connections
 mongoose.connect(
-  `mongodb://${process.env.DATABASEUSER}:${encodeURIComponent(process.env.DATABASEPASSWORD)}${process.env.DATABASEURL}`
+  `mongodb+srv://${process.env.DATABASEUSER}:${encodeURIComponent(
+    process.env.DATABASEPASSWORD
+  )}${process.env.DATABASEURL}`
 );
+
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
-mongoose.connection.on('error', (err) => {
+mongoose.connection.on("error", (err) => {
   console.error(`🚫 → ${err.message}`);
 });
 
 // READY?! Let's go!
 
-
 // import all of our models
-require('./models/Band');
-require('./models/Album');
-require('./models/User');
-require('./models/Pedal');
+require("./models/Band");
+require("./models/Album");
+require("./models/User");
+require("./models/Pedal");
 
 // Start our app!
-const app = require('./app');
-app.set('port', process.env.PORT || 7878);
-const server = app.listen(app.get('port'), () => {
+const app = require("./app");
+app.set("port", process.env.PORT || 7878);
+const server = app.listen(app.get("port"), () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
 
 // TEMP send email
-require('./handlers/mail');
+require("./handlers/mail");
