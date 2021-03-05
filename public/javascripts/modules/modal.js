fe29ca4b734a1632214a9d478dbe45a96d10ce70
Thomas Bullock <motbollox@gmail.com>
2021-03-06 10:24:08 +1100

    Accessibility fixes

--- public/javascripts/modules/modal.js
+++ public/javascripts/modules/modal.js
@@ -1,43 +1,40 @@
 function modal() {
-	const modal = document.getElementById('galleryModal');
+  const modal = document.getElementById("galleryModal");
 
-	function closeModal() {
-		modal.style.display = "none";
-		modalTitle.textContent = '';
-		modalBody.innerHTML = '';
-	}
+  function closeModal() {
+    modal.style.display = "none";
+    modalBody.innerHTML = "";
+  }
 
-	if(!modal) {
-		return;
-	}
+  if (!modal) {
+    return;
+  }
 
-	const galleryBtns = Array.from(document.querySelectorAll('.single__gallery-thumb img'));
-  const modalBody = document.querySelector('.modal__body');
-  const modalTitle = document.querySelector('.modal__title');
-  // const modalFooter = document.querySelector('.modal__footer');        
-  const close = document.querySelector('.modal__close');
+  const galleryBtns = Array.from(
+    document.querySelectorAll(".single__gallery-thumb img")
+  );
+  const modalBody = document.querySelector(".modal__body");
+  const close = document.querySelector(".modal__close");
 
-	galleryBtns.forEach( (btn, index) => {
-		btn.addEventListener('click', (e) => {
-			const imgString = e.target.src.split('/').pop();
-			const imgAlt = e.target.alt; 
-			const imgStringLg = `${imgString.split('_')[0]}_Lg.jpeg`;
-			const modalImg = document.createElement('img');
+  galleryBtns.forEach((btn, index) => {
+    btn.addEventListener("click", (e) => {
+      const imgString = e.target.src.split("/").pop();
+      const imgStringLg = `${imgString.split("_")[0]}_Lg.jpeg`;
+      const modalImg = document.createElement("img");
 
-			modalImg.src = `/uploads/${imgStringLg}`;
-      modalTitle.textContent = imgAlt;			
-			modalBody.appendChild(modalImg);
-			modal.style.display = "block";
-		})
-	})
+      modalImg.src = `/uploads/${imgStringLg}`;
 
-  close.addEventListener('click', closeModal);
-  window.addEventListener('click', function(e){
-      if (event.target == modal) {
-          closeModal()    
-      }            
-  });	
+      modalBody.appendChild(modalImg);
+      modal.style.display = "block";
+    });
+  });
 
+  close.addEventListener("click", closeModal);
+  window.addEventListener("click", function (e) {
+    if (event.target == modal) {
+      closeModal();
+    }
+  });
 }
 
 export default modal;
